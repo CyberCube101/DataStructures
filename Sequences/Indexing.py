a231a53fa1ecf2136a6cb0642fd19b834eb1a7d7
markb <markbanford@gmail.com>
2021-02-07 14:45:06 +0000

    Push 1

--- Sequences/Indexing.py
+++ Sequences/Indexing.py
@@ -1,3 +1,14 @@
+''' Python DataStructures
+
+List
+Dictionary
+Tuples
+Strings
+Sets
+Stacks
+
+'''
+
 x = 'computer'  # start:end+1:step - works on strings, lists and tuples
 
 print(x[1:4])
@@ -8,3 +19,14 @@ print(x[:-2])  # everything except last 2 items
 y = [7, 8, 3]
 for i, v in enumerate(y):  # returns index and item
     print(i, v)
+
+z = [2, 5, 8, 12]
+
+print(sum(z[-2:]))  # sum last 2 items
+
+### Lets say we want to sort by the second letter in each word
+
+words = ['Kevin', 'Kalvin', 'Korty', 'Kupton']
+
+new_list = sorted(words, key=lambda k: k[1])
+print(new_list)
