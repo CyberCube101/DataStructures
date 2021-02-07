4700bd81e72d2528d1d1bf2281127644950d6d1b
markb <markbanford@gmail.com>
2021-02-07 16:14:34 +0000

    Push 3

--- Sequences/Lists.py
+++ Sequences/Lists.py
@@ -26,3 +26,27 @@ print(y)
 ## list comprehension
 
 
+# pull numbers from a string
+
+s = ' I love 2 go t0 the store 7 times a week'
+
+nums = [x for x in s if x.isnumeric()]
+print(''.join(nums))
+
+# get index of a list item
+
+names = ['Cosmo', 'Pedro', 'Anu', 'Ray']
+idx = [k for k, v in enumerate(names) if v == 'Anu']
+print(idx)
+
+# delete item in list using comprehension
+
+letters = [x for x in 'ABCDEF']
+new_list = [a for a in letters if a != 'C']
+print(new_list)
+
+### if-else in a comprehension
+
+nums = [2, 4, 6, 8, 9, 10, 11]
+filtered = [x ** 2 if x % 2 == 0 else x ** 3 for x in nums]  # if even then square, else cube it
+print(filtered)
